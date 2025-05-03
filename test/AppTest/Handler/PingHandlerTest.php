923902f5638bf1a3ec6b1a4db0f45e2dea44c8c2
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-05-03 18:31:24 +0200

    ✅ Fix test

--- test/AppTest/Handler/PingHandlerTest.php
+++ test/AppTest/Handler/PingHandlerTest.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace AppTest\Handler;
 
-use App\Handler\PingHandler;
+use App\Handler\API\PingHandler;
 use Laminas\Diactoros\Response\JsonResponse;
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\Http\Message\ServerRequestInterface;
 
 class PingHandlerTest extends TestCase
 {
+    use ProphecyTrait;
+
     public function testResponse()
     {
         $pingHandler = new PingHandler();
